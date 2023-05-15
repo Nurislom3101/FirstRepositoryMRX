c7d0b70b2b20cb55c153252f02f1244c152a3eec
Nurislom3101 <ahmadjonovnurislom35@gmail.com>
2023-05-15 10:37:17 +0500

    first commit

---
+++ src/main/java/com/company/Magazine/Marketplace/MagazineMarketplaceApplication.java
@@ -0,0 +1,13 @@
+package com.company.Magazine.Marketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagazineMarketplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagazineMarketplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Magazine/Marketplace/MagazineMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Magazine.Marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazineMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
